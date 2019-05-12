6cea5119a35ae9c4f8db12ab07c60cd20df71ca6
Tony Cruz <tonycruz@Tonys-MBP.frontierlocal.net>
2019-05-12 18:39:33 -0500

    added new logic

--- letter.js
+++ letter.js
@@ -1,9 +1,32 @@
+var inquirer = require("inquirer");
+
 function Solution(word, guesses) {
     this.word = word;
-    this.guesses = guesses
+    this.guesses = guesses;
 }
-var newWords = [];
 var vayne = new Solution("vayne", 10);
 
-newWords.push(vayne);
-console.log (newWords);
+console.log(vayne.word.length);
+
+inquirer.prompt([
+    {
+        type: "input",
+        name: "letter",
+        message: "Guess a letter!"
+    }
+]).then(function(response){
+
+    console.log("You guessed " + response.letter + "!");
+    var current = vayne.word;
+    // var finalWord = current.replace(/[a-z]/g, "_");
+    // console.log(finalWord);
+    // console.log(vayne.word);
+    for (var i = 0; i < vayne.word.length; i++) {
+        if (response.letter === vayne.word[i]) {
+            console.log("True mate it works");
+            console.log(vayne.word)
+        } else {
+            console.log("Mate it dont work");
+        }
+    }
+}) 
