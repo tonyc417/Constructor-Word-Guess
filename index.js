3b2871d42835140c46d3ae64947f5253feff9a81
unknown <tonycruz417@gmail.com>
2019-05-12 15:36:27 -0500

    added inital files

---
+++ index.js
@@ -0,0 +1,3 @@
+var inquirer = require("inquirer");
+
+console.log("works");
---
+++ letter.js
@@ -0,0 +1,9 @@
+function Solution(word, guesses) {
+    this.word = word;
+    this.guesses = guesses
+}
+var newWords = [];
+var vayne = new Solution("vayne", 10);
+
+newWords.push(vayne);
+console.log (newWords);
